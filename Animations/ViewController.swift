b6a62dccfb0d056d7ad5e3eafdd6a0b19a7dfd02
Mari Melikishvili <marimelikishvili@maris-macbook-air.local>
2016-06-16 19:37:15 +0400

    Constraint animation demo

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -1,25 +1,107 @@
-//
-//  ViewController.swift
-//  Animations
-//
-//  Created by Mari Melikishvili on 6/15/16.
-//  Copyright © 2016 Mari Melikishvili. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var customView: UIView!
+    @IBOutlet weak var customViewCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet weak var customViewCenterYConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(animated: Bool) {
+        
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+    }
+    
+    func animate1() {
+        customViewCenterXConstraint.constant = -(view.frame.width - customView.frame.width) / 2
+        customViewCenterYConstraint.constant = -(view.frame.height - customView.frame.height) / 2
+        UIView.animateWithDuration(1.5) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func animate2() {
+        customViewCenterXConstraint.constant = (view.frame.width - customView.frame.width) / 2
+        customViewCenterYConstraint.constant = -(view.frame.height - customView.frame.height) / 2
+        UIView.animateWithDuration(0.5, animations: { 
+            self.view.layoutIfNeeded()
+        }) { (bool: Bool) in
+            let alert = UIAlertController(title: "Animation done", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func animate3() {
+        customViewCenterXConstraint.constant = (view.frame.width - customView.frame.width) / 2
+        customViewCenterYConstraint.constant = -(view.frame.height - customView.frame.height) / 2
+        
+        UIView.animateWithDuration(
+            0.8,
+            delay: 1,
+            options: [.CurveEaseInOut, .Autoreverse, .Repeat],
+            animations: {
+            self.view.layoutIfNeeded()
+        }) { (bool: Bool) in
+            
+        }
+        
+    }
+    
+    func animate4() {
+        customViewCenterXConstraint.constant = -(view.frame.width - customView.frame.width) / 2
+        customViewCenterYConstraint.constant = -(view.frame.height - customView.frame.height) / 2
+        
+        UIView.animateWithDuration(
+            1.5,
+            delay: 0,
+            usingSpringWithDamping: 0.45,
+            initialSpringVelocity: 5,
+            options: UIViewAnimationOptions.CurveEaseInOut,
+            animations: {
+            self.view.layoutIfNeeded()
+        }) { (bool: Bool) in
+            
+        }
+        
+    }
+    
+    func animate5() {
+        
+        UIView.animateWithDuration(2) {
+            self.customView.alpha = 0.5
+            self.customView.backgroundColor = UIColor.blueColor()
+            self.customView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI))
+            self.customView.transform = CGAffineTransformMakeScale(0.5, 0.5)
+        }
+        
+    }
+    
+    @IBAction func animateButtonTapped(sender: UIButton) {
+        animate5()
+    }
+    
+    func a() {
+        
+        UIView.animateWithDuration(1.5) {
+            
+        }
+        
+        UIView.animateWithDuration(0.5, animations: {
+            
+        }) { (bool: Bool) in
 
-
+        }
+        
+    }
 }
+
+
+
 
